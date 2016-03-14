f0fb64855de6b746ba2e09a6965a7de1105be058
David Miranda <david@storylog.com>
2016-03-13 23:25:04 -0400

    made prod env work

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('styles', function () {
 		.pipe(sass({
 			sourceComments: isProduction ? false : 'map'
 		})).on('error', handleError)
-		.pipe(isProduction ? concat() : gutil.noop()) // shouldnt this always run?
+		// .pipe(isProduction ? concat() : gutil.noop()) // shouldnt this always run?
 		.pipe(isProduction ? minifyCss() : gutil.noop())
 		.pipe(cachebust.references())
 		.pipe(cachebust.resources())
